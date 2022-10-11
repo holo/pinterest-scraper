e5eff11cfdad4c1839600eebec10bfa1a21eb512
alfie <24614772+holo@users.noreply.github.com>
2022-10-11 21:56:16 +0100

    fixed issues with ads

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let scrolls = 10;
+let scrolls = 1;
 let delay = 500; 
 let output = "";
 
 function scrape() {
 	let images = document.querySelectorAll('[data-test-id="non-story-pin-image"]');
 	for (image of images) {
-		let img = image.children[0].children[0].src.replace("236x", "originals")+"\n"
+		let img = image.children[0].children[0]
+		let link = img.src.replace("236x", "originals")+"\n"
 
-		if (output.includes(img)) {
+		if (output.includes(link) || !img.alt.includes("contains")) {
+			console.log(`skipped ${link}`)
 			continue
 		}
-		output += img;
+		output += link;
 	}
 }
 
